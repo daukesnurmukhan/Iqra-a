d463e9e023d4a45c830409a77da0c23adf866151
daukesnurmukhan <daukesnurmukhan@gmail.com>
2022-05-09 08:07:02 +0600

    Initial Commit

---
+++ Iqra'a/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Iqra'a
+//
+//  Created by Нұрмұхан Дәукес on 09.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Iqra'a/Iqra_aApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iqra_aApp.swift
+//  Iqra'a
+//
+//  Created by Нұрмұхан Дәукес on 09.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Iqra_aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
